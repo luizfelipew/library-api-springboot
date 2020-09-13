3e0b1ea433f291c265d72003b09f54334ab3edba
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-13 20:54:53 -0300

    finalizando os testes de integração de empréstimos

--- library-api/src/test/java/com/cursowendt/libraryapi/model/repository/LoanRepositoryTest.java
+++ library-api/src/test/java/com/cursowendt/libraryapi/model/repository/LoanRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -31,16 +33,40 @@ public class LoanRepositoryTest {
     @DisplayName("Deve verificar se existe o empréstimo não devolvido para o livro")
     public void existsByBookAndNotReturnedTest() {
         // given - cenario
+        Loan loan = createAndPersistLoan();
+        Book book = loan.getBook();
+
+        // when - execucao
+        Boolean exists = loanRepository.existsByBookAndNotReturned(book);
+
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve buscar o emprestimo pelo isbn do livro ou customer")
+    public void findByBookIsbnOrCustomer() {
+        // cenario
+        Loan loan = createAndPersistLoan();
+
+        //execucao
+        Page<Loan> result = loanRepository.findByBookIsbnOrCustomer("123", "Fulando", PageRequest.of(0, 10));
+
+        // verificacao
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).contains(loan);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
+    }
+
+    private Loan createAndPersistLoan() {
         final Book book = createNewBook("123");
         entityManager.persist(book);
 
         final Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
         entityManager.persist(loan);
 
-        // when - execucao
-        Boolean exists = loanRepository.existsByBookAndNotReturned(book);
-
-        assertThat(exists).isTrue();
+        return loan;
     }
 
 }

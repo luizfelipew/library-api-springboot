fea5fcdc37aa49c3488f68006c860529e74204d6
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-16 13:22:01 -0300

    implementando o serviço de agendamento de tarefas

--- library-api/src/main/java/com/cursowendt/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/com/cursowendt/libraryapi/LibraryApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,10 +15,10 @@ public class LibraryApiApplication {
         return new ModelMapper();
     }
 
-    @Scheduled(cron = "0 4 19 1/1 * ?")
-    public void testeAgendamentoTarefas() {
-		System.out.println("AGENDAMENTO DE TAREFAS FUNCIONANDO COM SUCESSO!");
-    }
+//    @Scheduled(cron = "0 4 19 1/1 * ?")
+//    public void testeAgendamentoTarefas() {
+//		System.out.println("AGENDAMENTO DE TAREFAS FUNCIONANDO COM SUCESSO!");
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApiApplication.class, args);

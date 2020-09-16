ecf8cd4d76129ca5451c116171c412ae382a6c08
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-16 16:12:28 -0300

    configurando o servidor MailTrap para envio de emails

---
+++ library-api/src/main/java/com/cursowendt/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.cursowendt.libraryapi.service.impl;
+
+import com.cursowendt.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMails(final String message, final List<String> mailsList) {
+
+    }
+
+}

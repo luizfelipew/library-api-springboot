9e5ee0bd981c114eda73067a2af53e3f0071d8c2
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-17 00:31:46 -0300

    testando o envio de emails

--- library-api/src/main/java/com/cursowendt/libraryapi/LibraryApiApplication.java
+++ library-api/src/main/java/com/cursowendt/libraryapi/LibraryApiApplication.java
@@ -1,19 +1,37 @@
 package com.cursowendt.libraryapi;
 
+import com.cursowendt.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
+
+//    @Autowired
+//    private EmailService emailService;
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+//
+//    @Bean
+//    public CommandLineRunner runner() {
+//        return args -> {
+//            List<String> emails = Arrays.asList("library-api-6128fe@inbox.mailtrap.io");
+//            emailService.sendMails("Testando serviço de emails.", emails);
+//            System.out.println("EMAILS ENVIADOS!!!");
+//        };
+//    }
 
 //    @Scheduled(cron = "0 4 19 1/1 * ?")
 //    public void testeAgendamentoTarefas() {

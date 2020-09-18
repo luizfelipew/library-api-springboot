429c6bffc1c7b640a1823d3eb3266a6691184730
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-18 19:26:19 -0300

    monitorando nossa aplicação Spring boot com o Spring boot Admin

---
+++ springboot-admin/src/test/java/com/adminwendt/springbootadmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,12 @@
+package com.adminwendt.springbootadmin;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
